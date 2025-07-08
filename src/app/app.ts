d5c8e5885c72740181e5a394cfac1277c2912f60
Cristian Flores <cristianflores.ee@gmail.com>
2025-07-08 11:45:16 -0400

    Customize PrimeNG breadcrumb with proper line wrapping

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
 import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [BreadcrumbModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
